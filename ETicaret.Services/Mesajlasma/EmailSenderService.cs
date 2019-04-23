bd674aab166bcead89514cade18172e67b08b853
Onur Gelmez <onurgelmez33@gmail.com>
2019-04-23 23:52:50 +0300

    EmailSenderService değişikliği
    SMTP Mail bilgileri veri tabanından gelecek

--- ETicaret.Services/Mesajlasma/EmailSenderService.cs
+++ ETicaret.Services/Mesajlasma/EmailSenderService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ETicaret.Services.System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -10,27 +11,38 @@ namespace ETicaret.Services.Mesajlasma
 {
     public class EmailSenderService : IEmailSenderService
     {
+        private ISettingService _settingService;
+        public EmailSenderService(ISettingService settingService)
+        {
+            _settingService = settingService;
+        }
         public EmailSendResult Send(EmailMessageModel model)
         {
+            var email = _settingService.GetSetting<string>("SmtpEmailAddress");
+            var senderName = _settingService.GetSetting<string>("EmailSenderName");
+            var password = _settingService.GetSetting<string>("SmtpEmailPassword");
+            var host = _settingService.GetSetting<string>("SmtpEmailHost");
+            var port = _settingService.GetSetting<int>("SmtpEmailPort");
+            var isSSL = _settingService.GetSetting<bool>("SmtpEmailSSL");
             var message = new MailMessage();
             foreach (var item in model.Receiver)
             {
                 message.To.Add(new MailAddress(item));
             }
-            message.From = new MailAddress("onur@noktanetakademi.com");
+            message.From = new MailAddress(email, senderName);
             message.Subject = model.Subject;
             message.Body = model.MessageBody;
             message.IsBodyHtml = true;
             SmtpClient smtp = new SmtpClient();
             var credential = new NetworkCredential
             {
-                UserName = "onur@noktanetakademi.com",
-                Password = "aJweox9XYpWq"
+                UserName = email,
+                Password = password
             };
             smtp.Credentials = credential;
-            smtp.Host = "mail.noktanetakademi.com";
-            smtp.Port = 587;
-            smtp.EnableSsl = false;
+            smtp.Host = host;
+            smtp.Port = port;
+            smtp.EnableSsl = isSSL;
             smtp.Send(message);
             return EmailSendResult.Success;
         }
